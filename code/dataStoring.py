1ca4c13125d71bf973fb4d88bff1b8849f625303
nix- <srezoski.nikica@gmail.com>
2017-09-11 21:07:17 +0200

    Reformatiranje i zapishuvanje na tekstot vo dokument.

--- code/dataStoring.py
+++ code/dataStoring.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python
+import csv
+#import tailer as tl
+
 
 # Function definition is here
-def printinfo( name, age ):
-   "This prints a passed info into this function"
-   print "Name: ", name
-   print "Age ", age
-   return;
+def printinfo(name, age):
+    "This prints a passed info into this function"
+    print "Name: ", name
+    print "Age ", age
+    return;
+
+
+def append_data(file_name, data):
+    with open(file_name, 'a') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(data)
+    return;
+
+
+def read_data(file_name):
+    with open(file_name, 'rb') as f:
+        reader = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
+        for row in reader:
+            print row
+    return;
+
+    # def read_data_tail(file_name):
+    #    file = open(file_name)
+    #    lastLines = tl.tail(file, 3)  # to read last 15 lines, change it  to any value.
+    #    file.close()
+    #    return lastLines;
--- code/spider.py
+++ code/spider.py
@@ -2,41 +2,54 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-from dataStoring import printinfo
+from dataStoring import append_data
+from dataStoring import read_data
+#from dataStoring import read_data_tail
 
-page = requests.get("https://en.spitogatos.gr/search/results/residential/sale/r108/m108m")
+def get_data_spitogatos():
+    page = requests.get("https://en.spitogatos.gr/search/results/residential/sale/r108/m108m")
 
-# Vraka status na pristap na stranata:
-# status code starting with a 2 generally indicates success, and a code starting with a 4 or a 5 indicates an error.
-print page.status_code
+    # Vraka status na pristap na stranata:
+    # status code starting with a 2 generally indicates success, and a code starting with a 4 or a 5 indicates an error.
+    print page.status_code
 
-soup = BeautifulSoup(page.content, 'html.parser')
-#print(soup.prettify())
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # print(soup.prettify())
 
-html = list(soup.children)[2]
-body = list(html.children)[3]
-#print list(body.children)
+    html = list(soup.children)[2]
+    body = list(html.children)[3]
+    # print list(body.children)
 
-# Zemanje na delot od HTML-to kaj sto 
-search_list = soup.find(id='searchDetailsMainContent')
-prices = search_list.find_all(class_='text xbig bold padding-right')
-locations = search_list.find_all(class_='text color dark_grey margin-bottom-small')
-area = search_list.find_all(class_='padding-right text medium')
+    # Zemanje na delot od HTML-to kaj sto
+    search_list = soup.find(id='searchDetailsMainContent')
+    prices = search_list.find_all(class_='text xbig bold padding-right')
+    locations = search_list.find_all(class_='text color dark_grey margin-bottom-small')
+    areas = search_list.find_all(class_='padding-right text medium')
 
-print prices[0].get_text() + '\t\t' + re.sub('[^0-9a-zA-Z]+', '', area[0].get_text())  + '\t' + locations[0].get_text()
-print prices[1].get_text() + '\t\t' + re.sub('[^0-9a-zA-Z]+', '', area[2].get_text())  + '\t' + locations[1].get_text()
-print prices[2].get_text() + '\t\t' + re.sub('[^0-9a-zA-Z]+', '', area[4].get_text())  + '\t' + locations[2].get_text()
-print prices[3].get_text() + '\t\t' + re.sub('[^0-9a-zA-Z]+', '', area[6].get_text())  + '\t' + locations[3].get_text()
-print prices[4].get_text() + '\t\t' + re.sub('[^0-9a-zA-Z]+', '', area[8].get_text())  + '\t' + locations[4].get_text()
-print prices[5].get_text() + '\t\t' + re.sub('[^0-9a-zA-Z]+', '', area[10].get_text()) + '\t' + locations[5].get_text()
-print prices[6].get_text() + '\t\t' + re.sub('[^0-9a-zA-Z]+', '', area[12].get_text()) + '\t' + locations[6].get_text()
-print prices[7].get_text() + '\t\t' + re.sub('[^0-9a-zA-Z]+', '', area[14].get_text()) + '\t' + locations[7].get_text()
-print prices[8].get_text() + '\t\t' + re.sub('[^0-9a-zA-Z]+', '', area[16].get_text()) + '\t' + locations[8].get_text()
-print prices[9].get_text() + '\t\t' + re.sub('[^0-9a-zA-Z]+', '', area[18].get_text()) + '\t' + locations[9].get_text()
+    # Get Timestamp
 
-printinfo('test' , '111')
+    #i = 0
+    #for i in range(10):
+    #    print prices[i].get_text() + '\t\t' + re.sub('[^0-9a-zA-Z]+', '', areas[i*2].get_text()) + '\t' + locations[
+    #        i].get_text()
+
+    # Reformatting the data
+    item_price = re.sub('[^0-9]+', '', prices[0].get_text())
+    item_area = re.sub('[^0-9a-zA-Z]+', '', areas[0*2].get_text())
+    item_location = ' '.join(locations[0].get_text().split())
+    # Appending the data to the tail of the file
+    append_data('data.csv', [item_price, item_area, item_location])
+
+    read_data('data.csv')
+
+    return;
+
+get_data_spitogatos();
+
+#print read_data_tail('data.csv')
 
 print('End')
+
 
 #print p.get_text()
 
