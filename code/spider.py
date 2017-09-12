8b36831cdfae184bf4d7a1cb2a33ff31958e5148
nix- <srezoski.nikica@gmail.com>
2017-09-12 21:00:20 +0200

    Akumuliranje na podatoci spored denovi.

--- code/spider.py
+++ code/spider.py
@@ -1,12 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import time
 
 from dataStoring import append_data
 from dataStoring import read_data
 from dataStoring import read_data_tail
 
-filename = './data.csv'
+timestr = time.strftime("%Y%m%d")
+print timestr
+
+filename = './data_'+timestr+'.csv'
 
 def get_data_spitogatos(filename):
     page = requests.get("https://en.spitogatos.gr/search/results/residential/sale/r108/m108m")
@@ -16,9 +20,9 @@ def get_data_spitogatos(filename):
     if page.status_code != 200:
         print 'Error accessing the web-site! [error code: ' + page.status_code + ' ]'
         return;
+        # print(soup.prettify())
 
     soup = BeautifulSoup(page.content, 'html.parser')
-    # print(soup.prettify())
 
     html = list(soup.children)[2]
     body = list(html.children)[3]
@@ -37,13 +41,27 @@ def get_data_spitogatos(filename):
     #    print prices[i].get_text() + '\t\t' + re.sub('[^0-9a-zA-Z]+', '', areas[i*2].get_text()) + '\t' + locations[
     #        i].get_text()
 
-    # Reformatting the data
-    item_price = re.sub('[^0-9]+', '', prices[0].get_text())
-    item_area = re.sub('[^0-9a-zA-Z]+', '', areas[0*2].get_text())
-    item_location = ' '.join(locations[0].get_text().split())
-    item_data = re.sub('[^0-9/]+', '', dates[0].get_text())
-    #item_link = links[0].get_text()
+    i = 0
+    for i in range(10):
+        # Reformatting the data
+        item_price = re.sub('[^0-9]+', '', prices[i].get_text())
+        item_area = re.sub('[^0-9a-zA-Z]+', '', areas[i*2].get_text())
+        item_location = ' '.join(locations[i].get_text().split())
+        item_data = re.sub('[^0-9/]+', '', dates[i].get_text())
+        #item_link = links[0].get_text()
+        # Appending the data to the tail of the file
+        append_data(filename, [item_price, item_area, item_location, item_data])
 
+        if int(item_price) <= 20000:
+            print 'todo: implement email notification!'
+
+    return;
+
+get_data_spitogatos(filename);
+
+print('End')
+
+'''
     if [item_price, item_area, item_location, item_data] == read_data_tail(filename):
         print 'Nothing to get.'
     else:
@@ -55,9 +73,4 @@ def get_data_spitogatos(filename):
         # Send notification
         if int(item_price) <= 20000:
             print 'todo: implement email notification!'
-
-    return;
-
-get_data_spitogatos(filename);
-
-print('End')
+'''
