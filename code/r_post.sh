4e386b32c07a203e4c3d90e7b3850d1c57c7a477
nix- <srezoski.nikica@gmail.com>
2017-09-29 01:21:56 +0200

    Brishenje na nepotrebnite fajlovi.

--- code/r_post.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script is executed every day at 7:30 AM.
-# note: Ako sakas da go smenis ova treba so comanda "crontab -e".
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # Get the location of the script
-
-cd $DIR
-sudo python daily_back_up.py
