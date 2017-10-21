92ca771136c3a6803b868a152a7c92b7dfe70336
nix- <srezoski.nikica@gmail.com>
2017-10-21 20:25:07 +0200

    Kreiranje na funkcija za generiranj na grafici.

--- code/daily_back_up.py
+++ code/daily_back_up.py
@@ -7,6 +7,7 @@ import os
 from datetime import date, timedelta
 from data_plot import plot_data
 from data_plot import get_data_graph
+from gen_area_graph import gen_graph_daily_avr
 
 from notifications import send_email_notification
 from data_handler import remove_duplicates
@@ -28,6 +29,7 @@ remove_duplicates(filename)
 print 'the code is starting'
 data = get_data_graph(filename)
 plot_data(data, './data/'+filename_grafik)
+gen_graph_daily_avr()
 send_email_notification(filename)
 
 copyfile('av_list.csv', './data/avr_data/gav_'+today.strftime("%Y%m%d")+'.csv')
