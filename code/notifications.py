9b83324179f14eb358ec3f8de41b2cd0f3d73286
nix- <srezoski.nikica@gmail.com>
2017-10-21 11:46:33 +0200

    Dodavanje na del za integriranje vo email na lista na grafici.

--- code/notifications.py
+++ code/notifications.py
@@ -29,7 +29,7 @@ def send_email(recivers, message):
 def send_email_notification(file_name):
 
     emails_list = "../../private/emails.csv"
-    chart = file_name.replace(" ", "").rstrip(file_name[-4:]) + '.png'
+    chart = "./data/" + file_name.replace(" ", "").rstrip(file_name[-4:]) + '.png'
     content = '\n'
 
     num_items = 0
@@ -67,18 +67,32 @@ def send_email_notification(file_name):
     msgText = MIMEText('This is the alternative plain text message.')
     msgAlternative.attach(msgText)
 
+    # todo: da se naprvi ciklus vo koj ke se vklucat site grafici
+    import glob
+    lista = glob.glob('./data/graph/graph_*.png')
+    lista = sorted(lista)
+    i = 0
+    img_id = ''
+    for item in lista:
+        i += 1
+        img_id += '<img src="cid:image%d"><br>' %i
+        #if (i % 2) == 0:
+        #    img_id += '<br>'
+
+
+
     # We reference the image in the IMG SRC attribute by the ID we give it below
     msgText = MIMEText('Dneven report/analiza na oglasi (izvor www.spitogatos.gr).<br> '
                        '<b>Analiza bazirana na %d oglasi.</b><br>'
                        '<img src="cid:image0"><br>'
+                       '%s'
                        '<b>Lista na filtrirani oglasi:</b> <br>'
                        '%s <br>'
                        '<b>note: Ako imate nekoja idea kako moze nesto podobro da se napravi..pisi te mi..</b>'
-                       % (num_items,content),
+                       % (num_items, img_id, content),
                        'html')
     msgAlternative.attach(msgText)
 
-    # This example assumes the image is in the current directory
     fp = open(chart, 'rb')
     msgImage = MIMEImage(fp.read())
     fp.close()
@@ -86,6 +100,21 @@ def send_email_notification(file_name):
     # Define the image's ID as referenced above
     msgImage.add_header('Content-ID', '<image0>')
     msgRoot.attach(msgImage)
+
+    lista = glob.glob('./data/graph/graph_*.png')
+    lista = sorted(lista)
+    i = 0
+    img_id = ''
+    for item in lista:
+        i += 1
+        # This example assumes the image is in the current directory
+        fp = open(item, 'rb')
+        msgImage = MIMEImage(fp.read())
+        fp.close()
+
+        # Define the image's ID as referenced above
+        msgImage.add_header('Content-ID', '<image%d>' %i)
+        msgRoot.attach(msgImage)
 
     with open(emails_list, 'rb') as f:
         email = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
